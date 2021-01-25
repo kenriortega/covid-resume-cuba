394030b780441d3a5c7f6eb38f54b82dc51517cc
kenriortega <kenriortega@gmail.com>
2021-01-24 20:19:34 -0500

    [REFACTOR] rename file for the new enpoint of the API

--- pages/api/latest.js
+++ pages/api/latest.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import latest from '../../data/latest.json'
 export default (req, res) => {
   res.statusCode = 200
-  res.json({ name: 'kenriortega' })
+  res.json({ ...latest })
 }
